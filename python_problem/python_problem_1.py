f7e8360cf36350b0b485e9687d15c851668ca651
slsl2 <kimminji7153@gmail.com>
2023-01-06 16:55:58 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,10 @@
 num = 0
 
-num = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+while True:
+    num = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+    if num.isdigit() == True and 1 <= int(num) <= 3:
+        break
+    elif num.isdigit() == False:
+        print('정수를 입력하세요')
+    else:
+        print('1, 2, 3 중 하나를 입력하세요')
