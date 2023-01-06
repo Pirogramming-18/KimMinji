11a27ce2f12164bcfc9c949bf294951464129620
slsl2 <kimminji7153@gmail.com>
2023-01-06 19:31:42 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,7 +1,7 @@
 num = 0
 
-def playerA():
-    global num
+def brGame():
+    global numbers
     while True:
         numbers = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
         if numbers.isdigit() == True and 1 <= int(numbers) <= 3:
@@ -11,35 +11,25 @@ def playerA():
         else:
             print('1, 2, 3 중 하나를 입력하세요')
     numbers = int(numbers)
+
+while True:
+    
+    brGame()
     for i in range(numbers):
         num+=1
         print('playerA :', num)
         if num == 31:
             break
-
-def playerB():
-    global num    
-    while True:
-        numbers = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
-        if numbers.isdigit() == True and 1 <= int(numbers) <= 3:
-            break
-        elif numbers.isdigit() == False:
-            print('정수를 입력하세요')
-        else:
-            print('1, 2, 3 중 하나를 입력하세요')
-    numbers = int(numbers)
+    if num == 31:
+        print('playerB win!')
+        break
+ 
+    brGame()
     for i in range(numbers):
         num+=1
         print('playerB :', num)
         if num == 31:
             break
-
-while True:
-    playerA()
-    if num == 31:
-        print('playerB win!')
-        break
-    playerB()
     if num == 31:
         print('playerA win!')
         break
