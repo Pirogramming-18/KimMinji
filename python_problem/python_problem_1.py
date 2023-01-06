cc074a87b8f932137619076d3a9492275c1e99b0
slsl2 <kimminji7153@gmail.com>
2023-01-06 18:49:39 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,29 +1,43 @@
 num = 0
 
-while True:
-    numbers = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
-    if numbers.isdigit() == True and 1 <= int(numbers) <= 3:
-        break
-    elif numbers.isdigit() == False:
-        print('정수를 입력하세요')
-    else:
-        print('1, 2, 3 중 하나를 입력하세요')
+def playerA():
+    global num
+    while True:
+        numbers = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+        if numbers.isdigit() == True and 1 <= int(numbers) <= 3:
+            break
+        elif numbers.isdigit() == False:
+            print('정수를 입력하세요')
+        else:
+            print('1, 2, 3 중 하나를 입력하세요')
+    numbers = int(numbers)
+    for i in range(numbers):
+        num+=1
+        print('playerA :', num)
+        if num == 31:
+            break
 
-numbers = int(numbers)
-for i in range(numbers):
-    num+=1
-    print('playerA :', num)
-    
-while True:
-    numbers = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
-    if numbers.isdigit() == True and 1 <= int(numbers) <= 3:
-        break
-    elif numbers.isdigit() == False:
-        print('정수를 입력하세요')
-    else:
-        print('1, 2, 3 중 하나를 입력하세요')
+def playerB():
+    global num    
+    while True:
+        numbers = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+        if numbers.isdigit() == True and 1 <= int(numbers) <= 3:
+            break
+        elif numbers.isdigit() == False:
+            print('정수를 입력하세요')
+        else:
+            print('1, 2, 3 중 하나를 입력하세요')
+    numbers = int(numbers)
+    for i in range(numbers):
+        num+=1
+        print('playerB :', num)
+        if num == 31:
+            break
 
-numbers = int(numbers)
-for i in range(numbers):
-    num+=1
-    print('playerB :', num)
+while True:
+    playerA()
+    if num == 31:
+        break
+    playerB()
+    if num == 31:
+        break
