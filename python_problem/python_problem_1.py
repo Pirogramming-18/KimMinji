72105ad8650aef015dea0ac0553c71d9f2cbc1e6
slsl2 <kimminji7153@gmail.com>
2023-01-06 17:54:15 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -10,6 +10,20 @@ while True:
         print('1, 2, 3 중 하나를 입력하세요')
 
 numbers = int(numbers)
-for num in range(numbers):
+for i in range(numbers):
     num+=1
     print('playerA :', num)
+    
+while True:
+    numbers = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+    if numbers.isdigit() == True and 1 <= int(numbers) <= 3:
+        break
+    elif numbers.isdigit() == False:
+        print('정수를 입력하세요')
+    else:
+        print('1, 2, 3 중 하나를 입력하세요')
+
+numbers = int(numbers)
+for i in range(numbers):
+    num+=1
+    print('playerB :', num)
