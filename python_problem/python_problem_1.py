cb919bcdd22a5275dc5331d1c0d91be5e5d03f8b
slsl2 <kimminji7153@gmail.com>
2023-01-06 17:29:24 +0900

    1.4

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,10 +1,15 @@
 num = 0
 
 while True:
-    num = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
-    if num.isdigit() == True and 1 <= int(num) <= 3:
+    numbers = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+    if numbers.isdigit() == True and 1 <= int(numbers) <= 3:
         break
-    elif num.isdigit() == False:
+    elif numbers.isdigit() == False:
         print('정수를 입력하세요')
     else:
         print('1, 2, 3 중 하나를 입력하세요')
+
+numbers = int(numbers)
+for num in range(numbers):
+    num+=1
+    print('playerA :', num)
