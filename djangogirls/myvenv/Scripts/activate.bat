fd4e9db1d7f49d295c344fb5ba3c4a85fdfad5e6
slsl2 <kimminji7153@gmail.com>
2023-01-11 15:03:19 +0900

    나의 첫 번째 Django 프로젝트!

--- djangogirls/myvenv/Scripts/activate.bat
+++ djangogirls/myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\김민지\Desktop\학교\2022\피로그래밍18기\파이썬코딩도장\KimMinji\djangogirls\myvenv
+set VIRTUAL_ENV=C:\Users\김민지\Desktop\230110\KimMinji\djangogirls\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,7 +25,6 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(myvenv) 
 
 :END
 if defined _OLD_CODEPAGE (
--- djangogirls/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\김민지\Desktop\학교\2022\피로그래밍18기\파이썬코딩도장\KimMinji\djangogirls\myvenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
