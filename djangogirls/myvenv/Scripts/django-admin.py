93670251063985a8856cb709859c0ae8623029a1
slsl2 <kimminji7153@gmail.com>
2023-01-11 12:42:09 +0900

    설치하기

---
+++ djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\김민지\Desktop\학교\2022\피로그래밍18기\파이썬코딩도장\KimMinji\djangogirls\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
