46553b707d463d78fcc18e508b6fd3a780665afd
Minji Kim <kimminji7153@gmail.com>
2023-01-14 01:04:38 +0900

    update 추가

--- myMovieReviews/server/apps/reviews/urls.py
+++ myMovieReviews/server/apps/reviews/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from server.apps.reviews.views import reviews_outline, reviews_create, reviews_details, reviews_delete
+from server.apps.reviews.views import reviews_outline, reviews_create, reviews_details, reviews_update, reviews_delete
 
 urlpatterns = [
     path("", reviews_outline),
     path("reviews/create", reviews_create),
     path("reviews/<int:pk>", reviews_details),
+    path("reviews/<int:pk>/update", reviews_update),
     path("reviews/<int:pk>/delete", reviews_delete),
 ]
--- myMovieReviews/server/apps/reviews/views.py
+++ myMovieReviews/server/apps/reviews/views.py
@@ -16,17 +16,33 @@ def reviews_details(request, pk, *args, **kwargs):
 def reviews_create(request, *args, **kwargs):
     if request.method == "POST":
         Review.objects.create(
-            title=request.POST["title"],
-            director=request.POST["director"],
-            main_actors=request.POST["main_actors"],
-            genre=request.POST["genre"],
-            year=request.POST["year"],
-            running_time=request.POST["running_time"],
-            rating=request.POST["rating"],
-            content=request.POST["content"],
+            title = request.POST["title"],
+            director = request.POST["director"],
+            main_actors = request.POST["main_actors"],
+            genre = request.POST["genre"],
+            year = request.POST["year"],
+            running_time = request.POST["running_time"],
+            rating = request.POST["rating"],
+            content = request.POST["content"],
         )
         return redirect("/")
     return render(request, "reviews/reviews_create.html")
+
+
+def reviews_update(request, pk, *args, **kwargs):
+    review = Review.objects.get(id=pk)
+    if request.method == "POST":
+        review.title = request.POST["title"]
+        review.director = request.POST["director"]
+        review.main_actors = request.POST["main_actors"]
+        review.genre = request.POST["genre"]
+        review.year = request.POST["year"]
+        review.running_time = request.POST["running_time"]
+        review.rating = request.POST["rating"]
+        review.content = request.POST["content"]
+        review.save()
+        return redirect(f"/reviews/{review.id}")
+    return render(request, "reviews/reviews_update.html", {"review": review})
 
 
 def reviews_delete(request, pk, *args, **kwargs):
