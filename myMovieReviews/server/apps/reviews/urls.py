30ebd2e045955db748ba9277a1e0bf6445fc63f0
Minji Kim <kimminji7153@gmail.com>
2023-01-14 05:53:18 +0900

    Challenge 프론트엔드

--- myMovieReviews/server/apps/reviews/urls.py
+++ myMovieReviews/server/apps/reviews/urls.py
@@ -1,3 +1,6 @@
+from django.conf.urls.static import static
+from django.conf import settings
+
 from django.urls import path
 
 from server.apps.reviews.views import reviews_outline, reviews_create, reviews_details, reviews_update, reviews_delete
@@ -9,3 +12,5 @@ urlpatterns = [
     path("reviews/<int:pk>/update", reviews_update),
     path("reviews/<int:pk>/delete", reviews_delete),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
